aabae237d78cf733b1e2ec2773bd21e659537129
DRobertson <dustinuriah@gmail.com>
2019-04-08 12:07:56 -0600

    add hover play event

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <v-app dark>
     <v-toolbar app light>
       <v-toolbar-side-icon>
         <v-img :src="require('../src/assets/witcher-logo-b.png')"></v-img>
@@ -7,7 +7,7 @@
 
       <v-toolbar-title class="headline text-uppercase">
         <span>The Witcher</span>
-        <span class="font-weight-light">Charcter Guide</span>
+        <span class="font-weight-light">Charcter Database</span>
       </v-toolbar-title>
 
       <v-spacer></v-spacer>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,21 @@
+// Play on hover
+
 <template>
   <v-container>
     <v-layout row wrap align-center justify-center fill-height>
       <v-flex xs12>
         <h2 class="pb-3">Characters</h2>
       </v-flex>
-      <v-flex v-for="c in characters" xs12 sm6 md4 lg3 xl2>
+      <v-flex v-for="c in characters" v-bind:key="c.id" m5 xs12 sm6 lg4 xl3>
         <h2>{{ c.name }}</h2>
-        <video
-          :src="`${publicPath}img/${c.imageUrl}`"
-          width="249"
-          height="357"
-          loop
-          type="video/webm"
-        />
+        <video 
+        :src="`${publicPath}img/${c.imageUrl}`" 
+        width="300" 
+        loop 
+        type="video/webm"
+        @mouseover="play()"
+        @mouseleave="pause()"
+      />
       </v-flex>
     </v-layout>
   </v-container>
@@ -25,13 +28,23 @@ import gql from "graphql-tag";
 export default {
   data() {
     return {
-      publicPath: process.env.BASE_URL
+      publicPath: process.env.BASE_URL,
+      videoElement: null,
+      paused: null
     };
   },
   mounted() {
     // return axios
     //   .get("https://pokeapi.co/api/v2/pokemon/1")
     //   .then(response => (this.info = response));
+  },
+  methods: {
+    play() {
+      event.target.play();
+    },
+    pause() {
+      event.target.pause();
+    }
   },
   apollo: {
     characters: gql`
